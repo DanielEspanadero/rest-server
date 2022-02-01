f63399d350559d05d7ef04397b1149d165c10b43
Daniel Españadero <developer@daniel-espanadero.com>
2022-02-01 12:42:30 +0100

    Validate all the necessary fields

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,6 @@
 const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
+
 const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
@@ -25,14 +26,22 @@ const usuariosPut = (req, res) => {
 };
 
 const usuariosPost = async (req, res) => {
+
+    
     const { nombre, correo, password, rol } = req.body;
     const usuario = new Usuario({ nombre, correo, password, rol });
 
     // Verificar si el correo existe.
+    const existeEmail = await Usuario.findOne({ correo });
+    if (existeEmail) {
+        return res.status(400).json({
+            msg: 'Ese correo ya está registrado'
+        })
+    }
 
     // Encriptar la contraseña
     const salt = bcryptjs.genSaltSync();
-    usuario.password = bcryptjs.hashSync(password,salt);
+    usuario.password = bcryptjs.hashSync(password, salt);
 
     // Guardar en BD
     await usuario.save();
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validarCampos } = require('../middlewares/validar-campos');
+
 const { usuariosGet,
     usuariosPut,
     usuariosPost,
     usuariosDelete
-} = require('../controllers/usuarios')
+} = require('../controllers/usuarios');
 
 const router = Router();
 
@@ -11,7 +15,13 @@ router.get('/', usuariosGet);
 
 router.put('/:id', usuariosPut);
 
-router.post('/', usuariosPost);
+router.post('/', [
+    check('nombre', 'El nombre es obligarotio').not().isEmpty(),
+    check('password', 'El password debe ser de más de 6 letras').isLength({ min: 6 }),
+    check('correo', 'El correo no es válido').isEmail(),
+    check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    validarCampos
+], usuariosPost);
 
 router.delete('/', usuariosDelete);
 
