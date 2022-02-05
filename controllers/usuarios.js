2f3c76f7e2e4b2470a0a82c24123e0bb3f7b5dc5
Daniel Españadero <developer@daniel-espanadero.com>
2022-02-05 17:41:44 +0100

    Return total number of records in a collection

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -4,12 +4,20 @@ const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 
 const usuariosGet = async (req = request, res = response) => {
-    const {limite = 5, desde = 0} = req.query;
-    const usuarios = await Usuario.find()
-    .skip(Number(desde))
-    .limit(Number(limite));
+    const { limite = 5, desde = 0 } = req.query;
+    const query = {estado:true};
 
-    res.json(usuarios)
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ])
+
+    res.json({
+        total,
+        usuarios
+    })
 };
 
 const usuariosPost = async (req, res) => {
