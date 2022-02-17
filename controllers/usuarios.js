e607526ae4c1f175b8e8314d638f4b04d2c9d2e8
Daniel Españadero <developer@daniel-espanadero.com>
2022-02-17 10:18:03 +0100

    Protect routes by using token

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -57,12 +57,14 @@ const usuariosDelete = async (req, res) => {
 
     const { id } = req.params;
 
+    const uid = req.uid;
+
     // Lo borramos fisicamente (NO RECOMENDABLE)
     // const usuario = await Usuario.findByIdAndDelete(id);
 
     const usuario = await Usuario.findByIdAndUpdate(id, { estado: false })
 
-    res.json(usuario)
+    res.json({usuario, uid})
 };
 
 
